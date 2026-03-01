ae6c77e67101361e2c9458d7163672b7583c70a0
C0ldSt33l <sychev2003di@gmail.com>
2026-03-01 22:56:30 +0300

    add data for testing

---
+++ lab2/non-linear classification.py
@@ -0,0 +1,10 @@
+from helpers.read_csv import get_data_from_csv
+from helpers.point import *
+
+def nonlinear_classification_test():
+    data
+    pass
+
+
+if __name__ == '__main__':
+    nonlinear_classification_test()
